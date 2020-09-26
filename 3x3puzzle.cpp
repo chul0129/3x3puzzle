308e783e58dcbdd45689f650c3d6f3db6e0c7123
chul0129 <yk2915@naver.com>
2020-09-26 17:23:53 +0900

    d

--- 3x3puzzle.cpp
+++ 3x3puzzle.cpp
@@ -1,73 +1,114 @@
-﻿#include <bangtal.h>
-using namespace bangtal;
+﻿#include<windows.h>
+#include <iostream>
+#include <bangtal.h>
+#include <ctime>
 
+using namespace bangtal;
+using namespace std;
 int main()
 {
-	
-	auto scene1 = Scene::create("시작", "image/원본.jpg");
-	auto scene2 = Scene::create("게임", "image/10.jpg");
-	auto button = Object::create("image/start.png", scene1,600,500);
-	int x0 = 274, x1 = 514, x2 = 754;
-	int y0 = 480, y1 = 240, y2 = 0;
-	int x[3] = { x0,x1,x2 };
-	int y[3] = { y0,y1,y2 };
-	setGameOption(GameOption::GAME_OPTION_INVENTORY_BUTTON, false);
-	setGameOption(GameOption::GAME_OPTION_MESSAGE_BOX_BUTTON, false);
+	srand(time(NULL));
 
-	 ObjectPtr puzzle[9] = {
-	 Object::create("image/1.jpg", scene2, x[0], y[0]),
-	 Object::create("image/2.jpg", scene2, x[1], y[0]),
-	 Object::create("image/3.jpg", scene2, x[2], y[0]),
-	 Object::create("image/4.jpg", scene2, x[0], y[1]),
-	 Object::create("image/5.jpg", scene2, x[1], y[1]),
-	 Object::create("image/6.jpg", scene2,x[2], y[1]),
-	 Object::create("image/7.jpg", scene2, x[0], y[2]),
-	 Object::create("image/8.jpg", scene2, x[1], y[2]),
+	auto scene = Scene::create("게임", "images/배경.jpg");
+	auto scene_start = Scene::create("시작", "images/원본.jpg");
+	auto button = Object::create("images/start.png", scene_start, 600, 300);
+	button->setScale(2);
+	ObjectPtr board[9];
+	ObjectPtr init_board[9];
+	button->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)-> bool {
+		scene->enter();
+		return true;
+		});
+	int blank = 8;
+	for (int i = 0; i < 9; i++)
+	{
 
-	};
+		string filename = "images/" + to_string(i + 1) + ".jpg";
 
-	
-	
+		board[i] = Object::create(filename, scene, 274 + (i % 3) * 240, 481 - (i / 3) * 240);
 
+		init_board[i] = board[i];
 
-	button->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		scene2->enter();
+		board[i]->setOnMouseCallback([&](ObjectPtr object, int, int, MouseAction action)->bool {
+			int j;
+			for (j = 0; j < 9; j++)
+			{
+				if (board[j] == object)
+					break;
+			}
+			if ((j % 3 > 0 && j - 1 == blank) || //왼쪽 조건
+				(j % 3 < 2 && j + 1 == blank) ||//오른쪽 조건
+				(j > 2 && j - 3 == blank) ||//위 조건
+				(j < 6 && j + 3 == blank))//아래 조건
+			{
+				board[j]->locate(scene, 274 + (blank % 3) * 240, 481 - (blank / 3) * 240);
+				board[blank]->locate(scene, 274 + (j % 3) * 240, 481 - (j / 3) * 240);
+
+				board[j] = board[blank];
+				board[blank] = object;
+				blank = j;
+				int k;
+				int p = 0;
+				for (k = 0; k < 9; k++) {
+
+					if (board[k] != init_board[k]) break;
+				}
+				if (k == 9)
+				{
+					showMessage("끝!");
+					scene_start->enter();
+				}
+
+			}
+			return true;
+			});
+	}
+	board[blank]->hide();
+
+	auto count = 0;
+	auto timer = Timer::create(0.1f);
+
+	timer->setOnTimerCallback([&](TimerPtr timer)->bool {
+		int j = 0;
+		int n = rand() % 4;
+		switch (n) {
+		case 0://좌
+			j = blank - 1; break;
+		case 1://우
+			j = blank + 1; break;
+		case 2: //위
+			j = blank - 3; break;
+		case 3://아래
+			j = blank + 3; break;
+
+		}
+		count++;
+		if (j > 0 && j < 9 && ((j % 4 > 0 && j - 1 == blank) || //왼쪽 조건
+			(j % 4 < 3 && j + 1 == blank) ||//오른쪽 조건
+			(j > 3 && j - 3 == blank) ||//위 조건
+			(j < 12 && j + 3 == blank)))
+		{
+			auto object = board[j];
+			board[j]->locate(scene, 271 + (blank % 3) * 240, 481 - (blank / 3) * 240);
+			board[blank]->locate(scene, 271 + (j % 3) * 240, 481 - (j / 3) * 240);
+
+			board[j] = board[blank];
+			board[blank] = object;
+			blank = j;
+		}
+		if (count < 100) {
+			timer->set(0.1f);
+			timer->start();
+		}
 
 		return true;
 		});
-	
-	
-	puzzle[0]->locate(scene2, x[2], y[0]);
-	puzzle[1]->locate(scene2, x[1], y[0]);
-	puzzle[2]->locate(scene2, x[0], y[0]);
-	puzzle[3]->locate(scene2, x[2], y[1]);
-	puzzle[4]->locate(scene2, x[1], y[1]);
-	puzzle[5]->locate(scene2, x[0], y[1]);
-	puzzle[6]->locate(scene2, x[2], y[2]);
-	puzzle[7]->locate(scene2, x[1], y[2]);
-	
 
-	for (int i = 0;i < 8;i++)
-	{
-		puzzle[i]->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-			
-			if (action == MouseAction::MOUSE_DRAG_RIGHT)
-				puzzle[7]->locate(scene2, x1 + 240, y2);
-			
-			
-			return true;
-			});
-
-	}
+	timer->start();
 
 
 
 
-
-
-
-
-	startGame(scene1);
+	startGame(scene_start);
 	return 0;
-
 }
